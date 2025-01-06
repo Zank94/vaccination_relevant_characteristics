6b1b1c08661d4925913f4ed3bf74af436d938c64
Mathieu Laporte <mlaporte@mesvaccins.net>
2025-01-06 12:36:16 +0100

    update build

--- builder/builders/html_builder.rb
+++ builder/builders/html_builder.rb
@@ -5,11 +5,15 @@ class HtmlBuilder
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/milligram/1.4.1/milligram.css"></head>'
   def build(conditions, output: $stdout)
+    version = `git describe`
+    info = JSON.parse(`git --no-pager log -1 --pretty=format:'{"commit": "%H", "tag": "%D", "author": "%an", "date": "%ad", "message": "%s"}' $(git describe --tags --abbrev=0)`, symbolize_names: true)
     output << "<html>"
     output << HEAD
-    output << '<body><div class="container">'
+    output << '<body><div class="container"><h1>Conditions dictionary</h1>'
+    output << "<details><summary>Version: #{version}</summary>
+      Commit: #{info[:commit]}<br>
+    </details>"
     conditions.each do |condition|
-      
       output << "<details id='C-#{condition['id']}'>
   <summary>C-#{condition['id']} - #{condition['label']}</summary>
       <pre><code>#{condition.to_yaml}</code></pre>
@@ -18,5 +22,3 @@ class HtmlBuilder
     output << "</div></body></html>"
   end
 end
-
-
